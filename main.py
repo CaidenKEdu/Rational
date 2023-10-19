597e9d3703870b202404ceeb44d5ca25538364e0
ckunitsky <ckunitsky@thegraysonschool.org>
2023-10-19 14:21:38 -0400

    Cleaned Up

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ class Ratio:
             self.b = b
             self.decimal = self.a/self.b
             self.mixednum = 0
+
     def lowest_terms(self):
         for i in range(2, 100000):
             if self.a % i == 0 and self.b % i == 0:
                 self.a = int(self.a / i)
                 self.b = int(self.b / i)
                 break
+
     def multiply(self, a):
         if type(a) != Ratio and type(a) != int:
             raise TypeError("Please Enter Required Value")
@@ -23,6 +25,7 @@ class Ratio:
             self.a = self.a * a.a
             self.b = self.b * a.b
             self.decimal = self.a / self.b
+
     def divide(self, a):
         if type(a) != Ratio and type(a) != int:
             raise TypeError("Please Enter Required Value")
@@ -33,6 +36,7 @@ class Ratio:
             self.a = self.a * a.b
             self.b = self.b * a.a
             self.decimal = self.a / self.b
+
     def add(self, a):
         if type(a) != Ratio and type(a) != int:
             raise TypeError("Please Enter Required Value")
@@ -45,6 +49,7 @@ class Ratio:
             self.a = (self.a * a.b) + (a.a * temp)
             self.lowest_terms()
             self.decimal = self.a / self.b
+
     def subtract(self, a):
         if type(a) != Ratio and type(a) != int:
             raise TypeError("Please Enter Required Value")
@@ -57,11 +62,13 @@ class Ratio:
             self.a = (self.a * a.b) - (a.a * temp)
             self.lowest_terms()
             self.decimal = self.a / self.b
+
     def inverse(self):
         temp = self.a
         self.a = self.b
         self.b = temp
         self.decimal = self.a / self.b
+
     def convert(self):
         return (self.a / self.b)
 
@@ -84,6 +91,7 @@ class Ratio:
                 self.decimal = self.a / self.b
                 self.a = None
                 self.b = None
+
     def percent(self):
             return((self.a/self.b)*100)
 
@@ -94,105 +102,8 @@ class Ratio:
                     self.mixednum = int(i / self.b)
                     self.a = self.a - int((i / self.b)*self.b)
 
-
-
 def main():
     print("Init")
     obj = Ratio(2, 6)
-    print(obj.a)
-    print(obj.b)
-    print("Lowest Terms")
-    obj.lowest_terms()
-    print(obj.a)
-    print(obj.b)
-    print("Multiply by a Ratio")
-    obj2 = Ratio(4,15)
-    obj.multiply(obj2)
-    print(obj.a)
-    print(obj.b)
-    print("Multiply by an Int")
-    obj.multiply(5)
-    print(obj.a)
-    print(obj.b)
-    print("Divide By Ratio")
-    obj.divide(obj2)
-    print(obj.a)
-    print(obj.b)
-    print("Lowest Terms")
-    obj.lowest_terms()
-    print(obj.a)
-    print(obj.b)
-    print("Divide By Int")
-    obj.divide(5)
-    print(obj.a)
-    print(obj.b)
-    print("Lowest Terms")
-    obj.lowest_terms()
-    print(obj.a)
-    print(obj.b)
-    print("Add A Ratio")
-    obj3 = Ratio(1,2)
-    obj.add(obj3)
-    print(obj.a)
-    print(obj.b)
-    print("Add An Int")
-    obj.add(5)
-    print(obj.a)
-    print(obj.b)
-    print("Subtract A Ratio")
-    obj.subtract(obj3)
-    print(obj.a)
-    print(obj.b)
-    print("Subtract An Int")
-    obj.subtract(5)
-    print(obj.a)
-    print(obj.b)
-    print("Lowest Terms")
-    obj.lowest_terms()
-    print(obj.a)
-    print(obj.b)
-    print("Inverse")
-    obj.inverse()
-    print(obj.a)
-    print(obj.b)
-    print("Inverse")
-    obj.inverse()
-    print(obj.a)
-    print(obj.b)
-    print("Decimal Convertion")
-    print(obj.convert())
-    print("Power Of Int")
-    obj.power(3)
-    print(obj.a)
-    print(obj.b)
-    print("Add An Int")
-    obj.add(5)
-    print(obj.a)
-    print(obj.b)
-    print("Lowest Terms")
-    obj.lowest_terms()
-    print(obj.a)
-    print(obj.b)
-    print("Power of Ratio, Rational")
-    obj4 = Ratio(36, 49)
-    obj4.power(obj3)
-    print(obj4.a)
-    print(obj4.b)
-    print("Power of Ratio, Non Rational")
-    obj.power(obj2)
-    print(obj.a)
-    print(obj.b)
-    print(obj.decimal)
-    print("Percentage")
-    print(obj4.percent())
-    print("Inverse")
-    obj4.inverse()
-    print(obj4.a)
-    print(obj4.b)
-    print("Mixed Fractions")
-    obj4.mixed()
-    print(obj4.mixednum)
-    print(obj4.a)
-    print(obj4.b)
 
 main()

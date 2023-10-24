9247045f4e80a0c2e2b4a4cdbeae3446986303fc
ckunitsky <ckunitsky@thegraysonschool.org>
2023-10-24 13:57:43 -0400

    Converted to Magic Methods

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ class Ratio:
     def __init__(self, a=1, b=1):
         if b == 0:
             raise ZeroDivisionError("Division By Zero!")
+        elif a != None or b != None:
+            self.a = a
+            self.b = b
+            self.decimal = self.a / self.b
+            self.mixednum = 0
         elif type(a) != int or type(b) != int:
             raise ValueError("Enter Integers Only")
         else:
@@ -10,91 +15,104 @@ class Ratio:
             self.b = b
             self.decimal = self.a/self.b
             self.mixednum = 0
+            self.lowest_terms()
 
     def __str__(self):
-        return str(self.a) + "/" + str(self.b)
+        if self.a == None or self.b == None:
+            return str(self.decimal)
+        else:
+            if self.b == 1:
+                return str(self.a)
+            return str(self.a) + "/" + str(self.b)
+
+    def __add__(self, a):
+        if type(a) != Ratio and type(a) != int:
+            raise TypeError("Please Enter Required Value")
+        elif type(a) == int:
+            temp_a = self.a + (self.b * a)
+            obj = Ratio(temp_a, self.b)
+        else:
+            temp = self.b
+            temp_b = self.b * a.b
+            temp_a = (self.a * a.b) + (a.a * temp)
+            obj = Ratio(temp_a, temp_b)
+        return obj
+
+    def __sub__(self, a):
+        if type(a) != Ratio and type(a) != int:
+            raise TypeError("Please Enter Required Value")
+        elif type(a) == int:
+            temp_a = self.a - (self.b * a)
+            obj = Ratio(temp_a, self.b)
+        else:
+            temp = self.b
+            temp_b = self.b * a.b
+            temp_a = (self.a * a.b) - (a.a * temp)
+            obj = Ratio(temp_a, temp_b)
+        return obj
+
+    def __truediv__(self, a):
+        if type(a) != Ratio and type(a) != int:
+            raise TypeError("Please Enter Required Value")
+        elif type(a) == int:
+            temp_b = self.b * a
+            obj = Ratio(self.a, temp_b)
+        else:
+            temp_a = self.a * a.b
+            temp_b = self.b * a.a
+            obj = Ratio(temp_a, temp_b)
+        return obj
+
+    def __mul__(self, a):
+        if type(a) != Ratio and type(a) != int:
+            raise TypeError("Please Enter Required Value")
+        elif type(a) == int:
+            temp_a = self.a * a
+            obj = Ratio(temp_a, self.b)
+        else:
+            temp_a = self.a * a.a
+            temp_b = self.b * a.b
+            obj = Ratio(temp_a, temp_b)
+        return obj
+
+    def __pow__(self, a, modulo = None):
+        if type(a) != Ratio and type(a) != int:
+            raise TypeError("Please Enter Required Value")
+        elif type(a) == int:
+            temp_a = self.a**a
+            temp_b = self.b**a
+            obj = Ratio(temp_a, temp_b)
+        else:
+            temp_a = (self.a**a.a)**(1/a.b)
+            temp_b = (self.b ** a.a) ** (1 / a.b)
+            temp = int(self.a)
+            temp2 = int(self.b)
+            if temp_a == temp and temp_b == temp2:
+                temp_a = int(self.a)
+                temp_b = int(self.b)
+                obj = Ratio(temp_a, temp_b)
+            else:
+                obj = Ratio(temp_a, temp_b)
+                obj.a = None
+                obj.b = None
+        return obj
 
     def lowest_terms(self):
         temp = gcd(self.a, self.b)
         self.a //= temp
         self.b //= temp
 
-    def multiply(self, a):
-        if type(a) != Ratio and type(a) != int:
-            raise TypeError("Please Enter Required Value")
-        elif type(a) == int:
-            self.a = self.a * a
-            self.decimal = self.a / self.b
-        else:
-            self.a = self.a * a.a
-            self.b = self.b * a.b
-            self.decimal = self.a / self.b
-
-    def divide(self, a):
-        if type(a) != Ratio and type(a) != int:
-            raise TypeError("Please Enter Required Value")
-        elif type(a) == int:
-            self.b = self.b * a
-            self.decimal = self.a / self.b
-        else:
-            self.a = self.a * a.b
-            self.b = self.b * a.a
-            self.decimal = self.a / self.b
-
-    def add(self, a):
-        if type(a) != Ratio and type(a) != int:
-            raise TypeError("Please Enter Required Value")
-        elif type(a) == int:
-            self.a = self.a + (self.b * a)
-            self.decimal = self.a / self.b
-        else:
-            temp = self.b
-            self.b = self.b * a.b
-            self.a = (self.a * a.b) + (a.a * temp)
-            self.lowest_terms()
-            self.decimal = self.a / self.b
-
-    def subtract(self, a):
-        if type(a) != Ratio and type(a) != int:
-            raise TypeError("Please Enter Required Value")
-        elif type(a) == int:
-            self.a = self.a - (self.b * a)
-            self.decimal = self.a / self.b
-        else:
-            temp = self.b
-            self.b = self.b * a.b
-            self.a = (self.a * a.b) - (a.a * temp)
-            self.lowest_terms()
-            self.decimal = self.a / self.b
-
-    def inverse(self):
+    def __invert__(self):
         temp = self.a
         self.a = self.b
         self.b = temp
         self.decimal = self.a / self.b
 
+    def __neg__(self):
+        self.a = -self.a
+
     def convert(self):
         return (self.a / self.b)
-
-    def power(self, a):
-        if type(a) != Ratio and type(a) != int:
-            raise TypeError("Please Enter Required Value")
-        elif type(a) == int:
-            self.a = self.a**a
-            self.b = self.b**a
-            self.decimal = self.a / self.b
-        else:
-            self.a = (self.a**a.a)**(1/a.b)
-            self.b = (self.b ** a.a) ** (1 / a.b)
-            temp = int(self.a)
-            temp2 = int(self.b)
-            if self.a == temp and self.b == temp2:
-                self.a = int(self.a)
-                self.b = int(self.b)
-            else:
-                self.decimal = self.a / self.b
-                self.a = None
-                self.b = None
 
     def percent(self):
             return((self.a/self.b)*100)
@@ -109,7 +127,9 @@ class Ratio:
 def main():
     print("Init")
     obj = Ratio(2, 6)
+    obj2 = Ratio(9, 20)
     print(obj)
+
 
 
 if __name__ == '__main__':
